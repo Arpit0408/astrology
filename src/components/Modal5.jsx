aa7d25a960036aabff87571e95be59f64fcdafb9
JainNeha7 <jneha0459@gmail.com>
2024-09-05 12:56:51 +0530

    Update Modal5.jsx

--- src/components/Modal5.jsx
+++ src/components/Modal5.jsx
@@ -6,7 +6,7 @@ function Modal5() {
 
   const [formValues, setFormValues] = useState({
     productinfo: 'LAMAFERA',
-    amount: '1666.50',
+    amount: '2222',
     firstname: '',
     name: '',
     email: '',
