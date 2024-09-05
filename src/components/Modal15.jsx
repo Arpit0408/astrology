f7a8dfd72811b8e373099cdb792282afcc82e996
JainNeha7 <jneha0459@gmail.com>
2024-09-05 13:03:16 +0530

    Update Modal15.jsx

--- src/components/Modal15.jsx
+++ src/components/Modal15.jsx
@@ -7,7 +7,7 @@ function Modal15() {
 
   const [formValues, setFormValues] = useState({
     productinfo: 'Holistic Treatement for Pregnancy',
-    amount: '5832.75',
+    amount: ' 7777',
     firstname: '',
     name: '',
     email: '',
