6582d3d0016bbf5f2fe1c34e0943a63be41a44bc
JainNeha7 <jneha0459@gmail.com>
2024-09-05 13:09:43 +0530

    Update Modal20.jsx

--- src/components/Modal20.jsx
+++ src/components/Modal20.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 function Modal20() {
   const [formValues, setFormValues] = useState({
     productinfo: 'Maa kali Group Healings',
-    amount: '833.25',
+    amount: '1111',
     firstname: '',
     name: '',
     email: '',
