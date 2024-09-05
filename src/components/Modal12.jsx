f1209844c8454c1815e10a5fd17d2c5a673c7fe3
JainNeha7 <jneha0459@gmail.com>
2024-09-05 13:01:01 +0530

    Update Modal12.jsx

--- src/components/Modal12.jsx
+++ src/components/Modal12.jsx
@@ -7,7 +7,7 @@ function Modal12() {
 
   const [formValues, setFormValues] = useState({
     productinfo: 'Maa Baglamukhi healing',
-    amount: '5832.75',
+    amount: '7777',
     firstname: '',
     name: '',
     email: '',
