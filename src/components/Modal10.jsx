b1864f3c7776248805632e362ccdae8798e0cadd
JainNeha7 <jneha0459@gmail.com>
2024-09-05 12:59:45 +0530

    Update Modal10.jsx

--- src/components/Modal10.jsx
+++ src/components/Modal10.jsx
@@ -6,7 +6,7 @@ function Modal10() {
 
   const [formValues, setFormValues] = useState({
     productinfo: 'SHALVIK WITH SHIVA HEALING',
-    amount: '4166.25',
+    amount: '5555',
     firstname: '',
     name: '',
     email: '',
